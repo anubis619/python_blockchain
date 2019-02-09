82d99758d3cdc543f8d44462c79bff8311012511
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-02-09 21:30:15 +0200

    Initial commit
    Blockchain start project.

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,41 @@
-blockchain = [[1]]
+# Initializing our blockchain list
+blockchain = []
 
 
-def getLastBlockchainValue():
+# Function definitions
+def get_blockchain_value():
+    """ Returns the last value of the current blockain. """
     return blockchain[-1]
 
 
-def addValue(transactionAmount):
-    blockchain.append([getLastBlockchainValue(), transactionAmount])
+def add_value(transaction_amount, last_transaction=[1]):
+    """Append a new value as well as the last blockchain value to the blockchain
+
+    
+    Arguments:
+        :transaction_amount: => The amout that should be added
+        :last_transaction: => The last blockchain value. Default ([1])
+    """
+    blockchain.append([last_transaction, transaction_amount])
 
 
-addValue(2)
-addValue(1232.212)
-addValue(48323.131)
+def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
+    return float(input("Please enter the amount of the transaction: "))
 
+
+# Code execution where user inputs transaction value
+tx_user_amount = get_user_input()
+add_value(tx_user_amount)
+
+
+tx_user_amount = get_user_input()
+add_value(last_transaction=get_blockchain_value(),
+         transaction_amount=tx_user_amount)
+
+
+tx_user_amount = get_user_input()
+add_value(tx_user_amount, get_blockchain_value())
+
+# Result of the code.
 print(blockchain)
