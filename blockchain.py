44fff20b97543f080a5f7d11eff6aecce3dba794
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-02-15 18:27:21 +0200

    Workflow update

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+import functools
+
 # Initializing our blockchain list
 MINING_REWARD = 100
 genesis_block = {
@@ -20,15 +22,17 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent=functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)    
+    # amount_sent = 0
+    # for tx in tx_sender:
+    #     if len(tx) > 0:
+    #         amount_sent += tx[0]
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received=functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
+    # amount_received = 0
+    # for tx in tx_recipient:
+    #     if len(tx) > 0:
+    #         amount_received += tx[0]
     return amount_received - amount_sent
     
 
@@ -166,7 +170,7 @@ while wait_for_input:
         print_blockchain_elements()
         print("Invalid hash! Code Exit")
         break  
-    print(get_balance('Alex'))
+    print('Balance of {}: {:6.2f}'.format('Alex', get_balance('Alex')))
 else:
     print('User left!')
 
